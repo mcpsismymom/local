4148426984d5f64c6f985baa585fcd19a05b9184
mcpsismymom <105395930+mcpsismymom@users.noreply.github.com>
2022-05-30 16:40:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+console.log("worked")
+
 if (document.cookie.includes('darkmode=1')) {
    var element = document.body;
    element.classList.toggle('darkmode');
 }
+
+console.log("worked2")
 function darkmode() {
    if (!document.cookie.includes('darkmode=1')) {
       document.cookie = 'darkmode=1; expires=Tue, Jan 1 12:00:00 AM EST 2030';
